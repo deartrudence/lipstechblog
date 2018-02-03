9c9b8ef942150c27c353c0228cd986461ce4e4a5
trudy macnabb <trudy.macnabb@gmail.com>
2018-02-03 20:51:01 +0100

    unified gradients hover effects

--- src/components/Tags/index.js
+++ src/components/Tags/index.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import './index.css'
+// import React from 'react'
+// import Link from 'gatsby-link'
+// import './index.css'
 
-const Tags = ({node}) => (
-    <div>
-        <div>
-            <h1 style={{
-                margin: 0,
-                fontFamily: 'Gruppo'
-            }}>
-                <Link
-                    className="tag"
-                    to={node.slug}
-                    style={{
-                        color: 'black',
+// const Tags = ({node}) => (
+//     <div>
+//         <div>
+//             <h1 style={{
+//                 margin: 0,
+//                 fontFamily: 'Gruppo'
+//             }}>
+//                 <Link
+//                     className="tag"
+//                     to={node.slug}
+//                     style={{
+//                         color: 'black',
                         
-                    }}
-                >
-                    tag ${node.title}
-                </Link>
-            </h1>
-        </div>
-    </div>
-)
+//                     }}
+//                 >
+//                     tag ${node.title}
+//                 </Link>
+//             </h1>
+//         </div>
+//     </div>
+// )
 
-export default Tags
+// export default Tags
